1c304a441f99f063f44f68220850be6210a90a65
alawaashez2 <mawasher@moj.gov.sa>
2026-03-29 16:49:41 +0300

    add filter tabs in markert screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2191138897"
+    serviceWorkerVersion: "3414929236"
   }
 });

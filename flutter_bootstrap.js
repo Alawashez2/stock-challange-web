f1281cdc509d852d237948af7e9e3148aa5b1be5
alawaashez2 <mawasher@moj.gov.sa>
2026-04-08 13:33:35 +0300

    version 0.3.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "322790191"
+    serviceWorkerVersion: "2179518882"
   }
 });
